fb4d46ff0dcd3fac254fc37543cb5266353a6661
Elizaveta <95237300+inwild86@users.noreply.github.com>
2022-05-23 23:18:59 +0400

    Add files via upload
    add test#2 and allure

--- src/main/java/site/stellarburgers/PO/MainPage.java
+++ src/main/java/site/stellarburgers/PO/MainPage.java
@@ -1,4 +1,5 @@
 package site.stellarburgers.PO;
+
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 import org.openqa.selenium.support.FindBy;
@@ -11,16 +12,16 @@ public class MainPage {
 
     public static final String MAIN_PAGE_URL = "https://stellarburgers.nomoreparties.site";
 
-    @FindBy(how = How.XPATH, using =  "//p[text()='Конструктор']")
+    @FindBy(how = How.XPATH, using = "//p[text()='Конструктор']")
     private SelenideElement constructor;
 
-    @FindBy(how = How.XPATH, using ="//p[text()='Лента Заказов']")
-    private SelenideElement OrderFeed;
+    @FindBy(how = How.XPATH, using = "//p[text()='Лента Заказов']")
+    private SelenideElement orderFeed;
 
-    @FindBy(how = How.XPATH, using ="//p[text()='Личный Кабинет']")
-    private SelenideElement PersonalArea;
+    @FindBy(how = How.XPATH, using = "//p[text()='Личный Кабинет']")
+    private SelenideElement personalArea;
 
-    @FindBy(how = How.XPATH, using ="//*[@class='AppHeader_header__logo__2D0X2']")
+    @FindBy(how = How.XPATH, using = "//*[@class='AppHeader_header__logo__2D0X2']")
     private SelenideElement linkLogo;
 
     @FindBy(how = How.XPATH, using = ".//button[text()='Войти в аккаунт']")
@@ -29,46 +30,58 @@ public class MainPage {
     @FindBy(how = How.XPATH, using = "//button[text()='Оформить заказ']")
     public SelenideElement createOrderButton;
 
-    @FindBy(how = How.XPATH, using =".//*[@class='text text_type_main-medium mb-6 mt-10' and text()='Начинки']")
+    @FindBy(how = How.XPATH, using = ".//*[@class='text text_type_main-medium mb-6 mt-10' and text()='Начинки']")
     private SelenideElement fillingsTab;
 
     @FindBy(how = How.XPATH, using = ".//h2[contains(text(),'Начинки')]")
     private SelenideElement fillingsButton;
 
-    @FindBy(how = How.XPATH, using ="//*[text()='Соусы']")
+    @FindBy(how = How.XPATH, using = "//*[text()='Соусы']")
     private SelenideElement saucesButton;
 
     @FindBy(how = How.XPATH, using = ".//*[@class='text text_type_main-medium mb-6 mt-10' and text()='Соусы']")
     private SelenideElement saucesTab;
 
-    @FindBy(how = How.XPATH, using =".//div[@class='tab_tab__1SPyG  pt-4 pr-10 pb-4 pl-10 noselect']")
+    @FindBy(how = How.XPATH, using = ".//div[@class='tab_tab__1SPyG  pt-4 pr-10 pb-4 pl-10 noselect']")
     private SelenideElement bunsButton;
-
-    @FindBy(how = How.XPATH, using =".//*[@class='text text_type_main-medium mb-6 mt-10' and text()='Булки']")
+    @FindBy(how = How.CLASS_NAME, using = "BurgerConstructor_basket__list__l9dp_")
+    private SelenideElement orderBasket;
+    @FindBy(how = How.XPATH, using = ".//*[@class='text text_type_main-medium mb-6 mt-10' and text()='Булки']")
     private SelenideElement bunsTab;
-
+    @FindBy(how = How.XPATH, using = "//p[text()='Сыр с астероидной плесенью']")
+    private SelenideElement lastIngredient;
+    @FindBy(how = How.XPATH, using = ".//p[text()='Соус Spicy-X']")
+    private SelenideElement sauceForDrop;
+    @FindBy(how = How.XPATH, using = ".//span[text()='Соус Spicy-X']")
+    private SelenideElement sauceInBasket;
 
     @Step("Click constructor button")
-    public void clickConstructor(){
+    public void clickConstructor() {
         constructor.click();
     }
 
     @Step("Click Order List button")
-    public  void clickOrderFeed(){    OrderFeed.click();    }
+    public void clickOrderFeed() {
+        orderFeed.click();
+    }
+
     @Step("Click sign in button")
-    public  static  void clicSignInButton(){    accountEntryButton.click();    }
+    public static void clicSignInButton() {
+        accountEntryButton.click();
+    }
 
     @Step("Click Personal Account button")
-    public void clickLinkPersonalArea(){
-        PersonalArea.click();
+    public void clickLinkPersonalArea() {
+        personalArea.click();
     }
 
     @Step("Click Link Logo button")
-    public void clickLinkLogo(){
+    public void clickLinkLogo() {
         linkLogo.click();
     }
+
     @Step("Checking the display of the Checkout button")
-    public  boolean CheckCreateOrderButton() {
+    public boolean checkCreateOrderButton() {
         return createOrderButton.isEnabled();
     }
 
@@ -90,8 +103,10 @@ public class MainPage {
 
     @Step("Check sauces tab exact text 'Соусы'")
     public boolean isSaucesTabText() {
-        saucesTab.shouldHave(exactText("Соусы"));
-        return true;
+        lastIngredient.scrollIntoView(true);
+        saucesButton.click();
+        sauceForDrop.dragAndDropTo(orderBasket);
+        return sauceInBasket.isDisplayed();
     }
 
     @Step("Click buns button")
